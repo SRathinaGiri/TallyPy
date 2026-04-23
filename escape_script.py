b0eeec2b9f0059cbd564bf9b4c547bc32682d98a
S Rathinagiri <srg@rathinagiri.in>
2026-04-23 23:23:40 +0530

    Delete escape_script.py

--- escape_script.py
+++
@@ -1,5 +0,0 @@
-import sys
-content = open(r'C:\Users\acer\OneDrive\MyProjects\Python\TallyXML\tally_stock_vouchers.py').read()
-escaped = content.replace('"', '""').replace('\n', '#(lf)')
-with open('escaped_python.txt', 'w', encoding='utf-8') as f:
-    f.write(escaped)
